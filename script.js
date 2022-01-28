1208f959291f491db000829d8ee5b0b2e5f9f051
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-28 09:10:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+
+let btn = document.querySelector('button');
+btn.addEventListener('click',async function(){
+  let stream = await navigator.mediaDevices.getDisplayMedia({
+    video:true
+    
+  })
